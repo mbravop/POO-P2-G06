e0abb460346883eb68d5e32226aa83ba61cf4f4c
mbravop <mbravop03@MacBook-Pro.local>
2022-06-23 14:42:05 -0500

    Creacion proyecto netbeans

---
+++ ProyectoPOO-G6/src/main/java/grupo6/proyectopoo/g6/ProyectoPOOG6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package grupo6.proyectopoo.g6;
+
+/**
+ *
+ * @author mbravop
+ */
+public class ProyectoPOOG6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
