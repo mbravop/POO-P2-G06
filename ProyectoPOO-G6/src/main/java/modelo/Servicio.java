640d53bc163939a45984bc9fc925ac0c1f8e7bf4
Dereck Santander <disantan@espol.edu.ec>
2022-07-09 08:42:14 -0500

    Implementación Clase Servicio

--- ProyectoPOO-G6/src/main/java/modelo/Servicio.java
+++ ProyectoPOO-G6/src/main/java/modelo/Servicio.java
@@ -9,5 +9,43 @@ package modelo;
  * @author mbravop
  */
 public class Servicio {
+    private String nombreServicio;
+    private int duracion;
+    private double precio;
+    private boolean estado;
     
+    public Servicio(String nS, int d, double p, boolean e){
+        nombreServicio=nS;
+        duracion=d;
+        precio=p;
+        estado=e;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+    
+    public String mostrarServicios(){
+        if (estado){
+            return "Servicio=" + nombreServicio + ", duracion=" + duracion +"minutos, precio=" + precio + ", estado= Activo";
+        } else {
+            return "Servicio=" + nombreServicio + ", duracion=" + duracion +"minutos, precio=" + precio + ", estado= Inactivo";
+        }
+    }
+    
+    public void agregarServicio(){
+        
+    }
+    
+    public void editarServicio(String nS, int d, double p){
+        nombreServicio=nS;
+        duracion=d;
+        precio= p;
+    }
+    
+    public void eliminarServicio(){
+        if (estado){
+            estado=false;
+        }
+    }
 }
