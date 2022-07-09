c39e1cff7f3b243f0664280ac7dd6aa6afce6ea4
Dereck Santander <disantan@espol.edu.ec>
2022-07-09 10:57:26 -0500

    Implementación del menu del sistema

--- ProyectoPOO-G6/src/main/java/grupo6/proyectopoo/g6/ProyectoPOOG6.java
+++ ProyectoPOO-G6/src/main/java/grupo6/proyectopoo/g6/ProyectoPOOG6.java
@@ -4,14 +4,51 @@
  */
 
 package grupo6.proyectopoo.g6;
+import java.util.Scanner;
 
-/**
- *
- * @author mbravop
- */
 public class ProyectoPOOG6 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner sc= new Scanner(System.in);
+        
+        boolean salir= false;
+        int opcion;
+        System.out.println("Menú del Sistema");
+        while(!salir){
+            System.out.println("1.Servicios");
+            System.out.println("2.Empleados");
+            System.out.println("3.Clientes");
+            System.out.println("4.Citas");
+            System.out.println("5.Atenciones");
+            System.out.println("6.Salir");
+            
+            System.out.println("Seleccione la opción que desee:");
+            opcion= sc.nextInt();
+            switch(opcion){
+                case 1:
+                    System.out.println("Servicios");
+                    break;
+                case 2:
+                    System.out.println("Empleados");
+                    break;
+                case 3:
+                    System.out.println("Clientes");
+                    break;
+                case 4:
+                    System.out.println("Citas");
+                    break;
+                case 5:
+                    System.out.println("Atenciones");
+                    break;
+                case 6:
+                    salir= true; 
+                    break;
+                default:
+                    System.out.println("Seleccion un número entre 1 y 6");
+            }
+        }
+        System.out.println("Ha salido del menú");
+        sc.close();
     }
-}
+}       
