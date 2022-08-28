b5115672c252172ea8e508e109797fa74a0314f1
Mauricio Bravo <82302737+mbravop@users.noreply.github.com>
2022-08-27 19:18:42 -0500

    Delete JuegoBingo directory

--- JuegoBingo/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module espol.edu.ec.juegobingo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens espol.edu.ec.juegobingo to javafx.fxml;
-    exports espol.edu.ec.juegobingo;
-}
